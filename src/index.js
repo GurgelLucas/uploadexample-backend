f5090cfb3b32706354c7929d4c1acc805de4e035
GurgelLucas <lgurgel13@gmail.com>
2019-05-02 02:42:00 -0300

    Adicionando a lib cors

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
+/**Database setup */
 mongoose.connect("mongodb://localhost:27017/upload", {
   useNewUrlParser: true
 });
 
+app.use(cors()); //deixando em branco está permitindo para qualquer cliente acessar a aplication
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
